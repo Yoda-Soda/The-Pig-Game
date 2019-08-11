d71e1d761894444e6d433a625b3db8714fe08bee
Jan Claasen <janclaasen@gmail.com>
2019-08-12 10:58:20 +1200

    Fixed bug where a new game doesn't display starter image

--- script/main.js
+++ script/main.js
@@ -28,26 +28,26 @@ function getPlayer(id) {
     Active: false,
     CurrentScore: 0,
     TotalScore: 0,
-    addCurrentScore: function (score) {
+    addCurrentScore: function(score) {
       this.CurrentScore += score;
       this.CurrentScoreBoard.innerHTML = this.CurrentScore;
     },
-    resetCurrentScore: function () {
+    resetCurrentScore: function() {
       this.CurrentScore = 0;
       this.CurrentScoreBoard.innerHTML = this.CurrentScore;
     },
-    hold: function () {
+    hold: function() {
       this.TotalScore += this.CurrentScore;
       this.TotalScoreBoard.innerHTML = this.TotalScore;
       this.resetCurrentScore();
       switchPlayers();
     },
-    isWinner: function () {
+    isWinner: function() {
       if (this.TotalScore + this.CurrentScore >= targetScore) {
         displayWinner();
       }
     },
-    fullReset: function () {
+    fullReset: function() {
       this.TotalScore = 0;
       this.CurrentScore = 0;
 
@@ -55,12 +55,12 @@ function getPlayer(id) {
       this.TotalScoreBoard.innerHTML = this.TotalScore;
       switchPlayers();
     },
-    enable: function () {
+    enable: function() {
       this.ActiveArea.style.backgroundColor = "#fff";
       this.HoldButton.style.visibility = "visible";
       this.RollButton.style.visibility = "visible";
     },
-    disable: function () {
+    disable: function() {
       this.ActiveArea.style.backgroundColor = "#f5f4f4";
       this.HoldButton.style.visibility = "hidden";
       this.RollButton.style.visibility = "hidden";
@@ -97,14 +97,14 @@ function addDice(diceCurrent) {
 }
 
 //checks if a player has reached a score of
-//100 and activates overlay else it's moves and adds the current score to the finalscore value
+//100 and activates overlay else its moves and adds the current score to the finalscore value
 function hold() {
   if (!currentPlayer.isWinner()) {
     currentPlayer.hold();
   }
 }
 
-//roll random dice roll between 1 and 6 and append it to it's image file name
+//roll random dice roll between 1 and 6 and append it to its image file name
 function rollDice() {
   var rand = Math.random();
   diceCurrent = Math.round(rand * 5) + 1;
@@ -112,7 +112,6 @@ function rollDice() {
     "images/dice-" + diceCurrent + ".png";
   addDice(diceCurrent);
 }
-
 
 //brings overlay to show winner
 function displayWinner() {
@@ -123,17 +122,15 @@ function displayWinner() {
 
 //resets all variables, applies those variables to dom and resets player area to player 1
 function newGame() {
-
   player1.fullReset();
   player2.fullReset();
 
   currentPlayer = player1;
   otherPlayer = player2;
 
-  diceImage.attributes.getNamedItem("src").nodeValue =
-    "images/dice-" + diceCurrent + ".png";
+  diceImage.attributes.getNamedItem("src").nodeValue = "images/dice-0.png";
 
-  // todo: is this useful?
+  // todo: is this useful? Yes this allows the winning overlay to be also used as a way to start the game just by clicking on it
   if (document.getElementById("overlay").style.display == "grid") {
     console.log("it is true");
     document.getElementById("overlay").style.display = "none";
